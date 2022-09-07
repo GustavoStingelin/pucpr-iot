1934a147d1b64d63ef1e5508f91c4dd5f3013c02
Gustavo Stingelin Cardoso Filho <gustavo.stingelin.c@outlook.com>
2022-09-07 20:01:09 -0300

    s8

--- definitions.py
+++ definitions.py
@@ -1,5 +1,18 @@
-user = ''
-password = ''
-client_id = '6549845616541'
-server = 'broker.mqttdashboard.com'
+import os
+
+server = 'mqtt.mydevices.com'
 port = 1883
+
+user = os.getenv('user')
+password = os.getenv('password')
+client_id = os.getenv('client_id')
+
+def topic_data(channel: str): return f"""v1/{user}/things/{client_id}/data/{channel}"""
+def topic_cmd(channel: str): return f"""v1/{user}/things/{client_id}/cmd/{channel}"""
+def topic_response(): return f"""v1/{user}/things/{client_id}/response"""
+
+
+temperature_topic_data = topic_data(0)
+humidity_topic_data = topic_data(1)
+heater_topic_cmd = topic_cmd(2)
+heater_topic_response = topic_response()
--- main.py
+++ main.py
@@ -2,18 +2,25 @@ import time
 
 import paho.mqtt.client as mqtt
 
-from definitions import client_id, user, password, port, server
+from definitions import client_id, user, password, port, server, temperature_topic_data, humidity_topic_data, \
+    heater_topic_cmd, heater_topic_response
 from hal import get_humidity, get_temperature, set_heater, HeaterState
 
 
+def handle_heater_message(client, user, message):
+    msg = message.payload.decode().split(',')
+    match msg[1]:
+        case '1':
+            set_heater(HeaterState.on)
+            client.publish(heater_topic_response, msg[0])
+        case '0':
+            set_heater(HeaterState.off)
+            client.publish(heater_topic_response, msg[0])
+
+
 def message_handler(client, user, message):
-    topic = message.topic
-    match topic:
-        case 'pucpr/iotmc/gu/heater':
-            msg = message.payload.decode()
-            match msg:
-                case 'on': set_heater(HeaterState.on)
-                case 'off': set_heater(HeaterState.off)
+    match message.topic:
+        case heater_topic_cmd: handle_heater_message(client, user, message)
 
 
 if __name__ == '__main__':
@@ -22,12 +29,12 @@ if __name__ == '__main__':
     client.connect(server, port)
 
     client.on_message = message_handler
-    client.subscribe('pucpr/iotmc/gu/heater')
+    client.subscribe(heater_topic_cmd)
     client.loop_start()
 
 while True:
-    client.publish('pucpr/iotmc/gu/temp', get_temperature())
-    client.publish('pucpr/iotmc/gu/hum', get_humidity())
-    time.sleep(1)
+    client.publish(temperature_topic_data, get_temperature())
+    client.publish(humidity_topic_data, get_humidity())
+    time.sleep(10)
 
 # client.disconnect()
